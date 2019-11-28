62ced356c37f8cbf662438883402919c0dbbdcb2
Aniket <aniket.bhatnagar0310@gmail.com>
2019-11-27 21:20:10 -0500

    Try  ridge regression

--- lasso_regression.py
+++ lasso_regression.py
@@ -1,6 +1,12 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
+import os
+import numpy as np
+import pandas as pd
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.utils import compute_class_weight
 
 if __name__ == '__main__':
     # Constants and file names
@@ -13,11 +19,25 @@ if __name__ == '__main__':
     data_labels = data['target']
 
     # Define feature extractor
-    vectorizer = TfidfVectorizer(min_df=0.01, max_df=0.7)  # Ignore words where document freq is lt 1% or gt 70%.
+    vectorizer = TfidfVectorizer(min_df=0.0005)  # Ignore words where document freq is lt 1% or gt 70%.
 
     # Train vectorizer on the text data
     vectorizer.fit(data_text)
+    all_features = vectorizer.transform(data_text)
     print ("Length of vocabulary of vectorizer", len(vectorizer.vocabulary_))
 
     # Do train test split
-    train_test_split()
+    X_train, X_val, y_train, y_val = train_test_split(
+        all_features, data_labels, random_state=3, test_size=0.3
+    )
+
+    # # Compute class weights
+    class_weight = compute_class_weight('balanced', np.unique(y_train), y_train)
+
+    # Ridge regression
+    lr = LogisticRegression(class_weight=class_weight, random_state=5)
+    lr.fit(X_train, y_train)
+    y_preds = lr.predict(X_val)
+
+    print ("Precision", precision_score(y_val, y_preds))
+    print ("Recall", recall_score(y_val, y_preds))
